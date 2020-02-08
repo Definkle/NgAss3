c46ae4c251b86cf69aeadbd37e5735e035cda83f
Definkle <52527078+Definkle@users.noreply.github.com>
2020-02-08 20:20:51 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgAss3';
+  items = [];
+  display = false;
+  count = 1;
+  onClick(){
+    this.display = !this.display;
+    this.items.push(this.count);
+    this.count++;
+  }
+  getBackground(data:any){
+    let index = this.items.indexOf(data);
+    return index > 3 ? 'blue' : '';
+  }
+  getColor(data:any){
+    let index = this.items.indexOf(data);
+    return index > 3 ? 'white' : '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    NgbModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
